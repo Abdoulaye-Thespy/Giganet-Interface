bd8ef822cf263260eb1d100523345618cb458960
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-12-23 16:41:20 +0100

    added more details to the connect page

--- index.js
+++ index.js
@@ -6,3 +6,28 @@
         }, 3700);
 
     }
+
+
+
+
+const delete_connect = () => {
+let btnDisconnect = document.getElementById("stop");
+let connect = document.getElementById("start");
+let conDiv = document.getElementById("con");
+let deconDiv = document.getElementById("decon");
+  btnDisconnect.style.display =  "none";
+  con.style.display =  "none";
+  decon.style.display =  "block";
+  connect.style.display = "block";
+}
+
+const delete_deconnect = () => {
+let btnDisconnect = document.getElementById("stop");
+let connect = document.getElementById("start");
+let conDiv = document.getElementById("con");
+let deconDiv = document.getElementById("decon");
+  btnDisconnect.style.display =  "block";
+  conDiv.style.display =  "block";
+  deconDiv.style.display =  "none";
+  connect.style.display = "none";
+}
